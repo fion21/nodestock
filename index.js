f76233dd59758320cf4b5407cb740de7535a2d26
Fion L <hwindepth@yahoo.com>
2021-02-11 11:23:15 +0000

    semi final

--- index.js
+++ index.js
@@ -2,33 +2,67 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-// set handlebars middleware
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// API KEY pk_062031d20883444f9ea74e2610fe2011
+// create call_api function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_8a833d42be9c4a3e908f28f0a51f5d61', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
+
+// Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+const otherstuff = "hello there, this is other stuff!";
 
-//other variable
-const otherstuff = "hello there, this is other stuff";
- 
-// set handlebar routes
+// Set handlebar index GET route
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: otherstuff
-    });
+	call_api(function(doneAPI) {
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, "fb");
+		
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			//console.log(doneAPI);
+			//posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+		
 });
 
 
-// set static folder
+
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
+});
+
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
-
-
-
-
-app.listen(PORT, () => console.log('Server is listening on port ' + PORT));
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
